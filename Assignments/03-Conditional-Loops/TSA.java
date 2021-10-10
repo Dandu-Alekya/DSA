f46e3959b9fd6e2e2a77c1c71b9113d1e6e5bb2d
Alekya Dandu <69137526+Dandu-Alekya@users.noreply.github.com>
2021-10-10 20:40:02 +0530

    Fixed typo in Java basic question 20

--- Assignments/03-Conditional-Loops/TSA.java
+++ Assignments/03-Conditional-Loops/TSA.java
@@ -4,7 +4,7 @@ public class TSA {
     public static void main(String[] args) {
         float a;
         Scanner sc = new Scanner(System.in);
-        //Total Surface Area Of Cube
+        //Q.20 Total Surface Area Of Cube
         System.out.println("Enter the side of the sqaure");
         a = sc.nextFloat();
         System.out.printf("The TSA of the Square is: %.2f\n",6*Math.pow(a,2));
