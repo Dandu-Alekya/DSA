38d84c8fab2b0a030c86d75b02a192d21ca6e936
Alekya Dandu <69137526+Dandu-Alekya@users.noreply.github.com>
2021-10-10 20:38:58 +0530

    Added solution for Java basic Q. 15

---
+++ Assignments/03-Conditional-Loops/TSA.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TSA {
+    public static void main(String[] args) {
+        float a;
+        Scanner sc = new Scanner(System.in);
+        //Total Surface Area Of Cube
+        System.out.println("Enter the side of the sqaure");
+        a = sc.nextFloat();
+        System.out.printf("The TSA of the Square is: %.2f\n",6*Math.pow(a,2));
+        sc.close();
+        
+    }
+    
+}
